af0c96a734e150dab950b90c42a1d8ca2f9cefe9
Caio <caio_mitsuo@hotmail.com>
2025-03-12 17:51:12 -0300

    First commit

---
+++ venda/src/test/kotlin/com/caiomitsuo/venda/VendaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.caiomitsuo.venda
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VendaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
